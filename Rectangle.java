7a4f72868d15f1d982c440f0a792c1445d77d818
MSaribuga <mustafasaribuga2018@gmail.com>
2025-04-17 13:16:42 +0300

    week8

---
+++ Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle extends Shape{
+    private int sideA= 0, sideB=0;
+
+    public Rectangle(int sideA, int sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public Rectangle(String color, int sideA, int sideB) {
+        super(color);
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+}
---
+++ testShape.java
@@ -0,0 +1,7 @@
+public class testShape {
+    public static void main(String[] args) {
+        Shape square = new Shape(  "blue");
+        square.describe(1,1);
+
+    }
+}
